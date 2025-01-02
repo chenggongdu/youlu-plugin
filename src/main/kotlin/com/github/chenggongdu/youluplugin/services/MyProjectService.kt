27f4d9e7548640059b46c8749abed72134cf22f9
GitHub Action <action@github.com>
2025-01-02 01:35:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/chenggongdu/youluplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chenggongdu/youluplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.chenggongdu.youluplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chenggongdu.youluplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
